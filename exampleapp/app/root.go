9fd204d34bf1657dd624509013df53e06fa7c3a8
alex <alex@cloudware.it>
2013-05-26 15:55:17 +0200

    Correct  Api Explorer base URL for both dev and appspot

--- exampleapp/app/root.go
+++ exampleapp/app/root.go
@@ -1,42 +1,47 @@
 package app
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"path/filepath"
+
+	"appengine"
+)
+
+const (
+	apiExplorerUrl = "https://developers.google.com/apis-explorer/"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, welcomeMsg)
+	t, err := template.ParseFiles(filepath.Join("templates", "home.html"))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	data := &struct {
+		BaseUrl        string
+		ApiExplorerUrl string
+	}{
+		BaseUrl:        getScheme(r) + "://" + r.Host,
+		ApiExplorerUrl: apiExplorerUrl,
+	}
+	if err := t.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+}
+
+func getScheme(r *http.Request) string {
+	scheme := r.URL.Scheme
+	if scheme == "" {
+		if appengine.IsDevAppServer() {
+			scheme = "http"
+		} else {
+			scheme = "https"
+		}
+	}
+	return scheme
 }
 
 func init() {
 	http.HandleFunc("/", handler)
 }
-
-const welcomeMsg = `
-<html>
-	<head><title>Go Endpoints</title></head>
-	<body>
-		<h1>Go Endpoints</h1>
-		<p>Hello, gopher! Try this:</p>
-
-		<ul>
-			<li><a href="/_ah/api/discovery/v1/apis" target="_blank">
-				/_ah/api/discovery/v1/apis
-			</a></li>
-			<li><a href="/_ah/api/discovery/v1/apis/greeting/v1/rest" target="_blank">
-				/_ah/api/discovery/v1/apis/greeting/v1/rest
-			</a></li>
-		</ul>
-		
-		<iframe src="https://developers.google.com/apis-explorer/?base=http://localhost:8080/_ah/api#p/greeting/v1/" style="width: 100%; height: 600px; border: none;"></iframe>
-		<!--<iframe src="https://developers.google.com/apis-explorer/?base=https://go-endpoints.appspot.com/_ah/api#p/greeting/v1/" style="width: 100%; height: 600px; border: none;"></iframe>-->
-
-		<hr/>
-		Source code:
-		<a href="https://github.com/crhym3/go-endpoints" target="_blank">
-			github.com/crhym3/go-endpoints
-		</a>
-	</body>
-</html>
-`
