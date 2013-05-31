d529c5ba9fb8a80097858811217b1f553d639f65
alex <alex@cloudware.it>
2013-05-31 14:47:01 +0200

    Addressing comments in commits for #2
      - accept endpoints.Context (was *http.Request) in CurrentBearerTokenUser
        and CurrentBearerTokenScope methods;
      - auth context implementation is now split in two parts: public interface
        and cachingContext (default implementation);
      - oauthResponseCache of cachingContext is now mutexed
    
    @dhermes what do you think?

--- endpoints/auth.go
+++ endpoints/auth.go
@@ -1,14 +1,12 @@
 package endpoints
 
 import (
+	"errors"
 	"net/http"
 	"sync"
 
 	"appengine"
 	"appengine/user"
-	"errors"
-
-	pb "appengine_internal/user"
 )
 
 const (
@@ -23,113 +21,67 @@ const (
 
 var (
 	ctxsMu sync.Mutex
-	ctxs   = make(map[*http.Request]*CachingContext)
+	ctxs   = make(map[*http.Request]Context)
+
+	// ContextFactory takes an in-flight HTTP request and creates a new
+	// context. 
+	// 
+	// It is a variable on purpose. You can set it to a stub implementation
+	// in tests.
+	ContextFactory func(*http.Request) Context
 )
 
-// CachingContext represents the context of an in-flight API request.
-// Also, it contains appengine.Context, so can be used in standard appengine/*
-// packages.
-type CachingContext struct {
+// Context represents the context of an in-flight API request.
+// It embeds appengine.Context so you can use with any other appengine/*
+// package methods.
+type Context interface {
 	appengine.Context
-	// map keys are scopes
-	oauthResponseCache map[string]*pb.GetOAuthUserResponse
+
+	// CurrentOAuthClientID returns a clientId associated with the scope.
+	CurrentOAuthClientID(scope string) (string, error)
+
+	// CurrentOAuthUser returns a user of this request for the given scope.
+	// It caches OAuth info at the first call for future invocations.
+	// 
+	// Returns an error if data for this scope is not available.
+	CurrentOAuthUser(scope string) (*user.User, error)
 }
 
 // NewContext returns a new context for an in-flight API (HTTP) request.
-func NewContext(req *http.Request) *CachingContext {
+func NewContext(req *http.Request) Context {
 	ctxsMu.Lock()
 	defer ctxsMu.Unlock()
-	cachingCtx, ok := ctxs[req]
+	c, ok := ctxs[req]
 
 	if !ok {
-		// TODO(dhermes): Fail if ctx is nil.
-		ctx := appengine.NewContext(req)
-		cachingCtx = &CachingContext{ctx, map[string]*pb.GetOAuthUserResponse{}}
-		ctxs[req] = cachingCtx
+		c = ContextFactory(req)
+		ctxs[req] = c
 	}
 
-	return cachingCtx
+	return c
 }
 
-// PopulateOAuthResponse updates (overwrites) OAuth user data associated with
-// this request and the given scope.
-func (ctx *CachingContext) PopulateOAuthResponse(scope string) error {
-	// Only one scope should be cached at once, so we just destroy the cache
-	ctx.oauthResponseCache = map[string]*pb.GetOAuthUserResponse{}
-
-	req := &pb.GetOAuthUserRequest{Scope: &scope}
-	res := &pb.GetOAuthUserResponse{}
-
-	err := ctx.Call("user", "GetOAuthUser", req, res, nil)
-	if err != nil {
-		return err
-	}
-
-	ctx.oauthResponseCache[scope] = res
-	return nil
-}
-
-// CachedCurrentOAuthClientID returns a clientId associated with the scope.
-func (ctx *CachingContext) CachedCurrentOAuthClientID(scope string) (*string, error) {
-	res, ok := ctx.oauthResponseCache[scope]
-
-	if !ok {
-		err := ctx.PopulateOAuthResponse(scope)
-		if err != nil {
-			return nil, err
-		}
-		res = ctx.oauthResponseCache[scope]
-	}
-
-	result := res.GetClientId()
-	return &result, nil
-}
-
-// CachedCurrentOAuthUser returns a user of this request for the given scope.
-// It caches OAuth info at the first call for future invocations.
-// 
-// Returns an error if data for this scope is not available.
-func (ctx *CachingContext) CachedCurrentOAuthUser(scope string) (*user.User, error) {
-	res, ok := ctx.oauthResponseCache[scope]
-
-	if !ok {
-		err := ctx.PopulateOAuthResponse(scope)
-		if err != nil {
-			return nil, err
-		}
-		res = ctx.oauthResponseCache[scope]
-	}
-
-	return &user.User{
-		Email:      *res.Email,
-		AuthDomain: *res.AuthDomain,
-		Admin:      res.GetIsAdmin(),
-		ID:         *res.UserId,
-	}, nil
-}
-
-// CurrentBearerTokenScope compares given scopes and clientIDs with those in ctx.
+// CurrentBearerTokenScope compares given scopes and clientIDs with those in c.
 // Both scopes and clientIDs args must have at least one element.
 // Returns a single scope (one of provided scopes) IIF two conditions are met:
-//   - it is found in ctx
-//   - client ID on that scope matches one of provided clientIDs from the args
-func CurrentBearerTokenScope(ctx *CachingContext, scopes []string, clientIDs []string) (*string, error) {
-	var clientID *string
+//   - it is found in Context c
+//   - client ID on that scope matches one of clientIDs in the args
+func CurrentBearerTokenScope(c Context, scopes []string, clientIDs []string) (string, error) {
 	for _, scope := range scopes {
-		clientID, _ = ctx.CachedCurrentOAuthClientID(scope)
-		if clientID == nil {
+		clientID, err := c.CurrentOAuthClientID(scope)
+		if err != nil {
 			continue
 		}
 
 		for _, id := range clientIDs {
-			if id == *clientID {
-				return &scope, nil
+			if id == clientID {
+				return scope, nil
 			}
 		}
 		// If none of the client IDs matches, return nil
-		return nil, errors.New("Mismatched Client ID")
+		return "", errors.New("Mismatched Client ID")
 	}
-	return nil, errors.New("No valid scope.")
+	return "", errors.New("No valid scope")
 }
 
 // CurrentBearerTokenUser returns a user associated with the request.
@@ -137,18 +89,11 @@ func CurrentBearerTokenScope(ctx *CachingContext, scopes []string, clientIDs []s
 // Returns an error if a client did not make a valid request, or none of
 // clientIDs are allowed to make requests, or user did not authorized any of
 // the scopes.
-func CurrentBearerTokenUser(req *http.Request, scopes []string, clientIDs []string) (*user.User, error) {
-	ctx := NewContext(req)
-	scope, err := CurrentBearerTokenScope(ctx, scopes, clientIDs)
-	if scope == nil {
+func CurrentBearerTokenUser(c Context, scopes []string, clientIDs []string) (*user.User, error) {
+	scope, err := CurrentBearerTokenScope(c, scopes, clientIDs)
+	if err != nil {
 		return nil, err
 	}
 
-	return ctx.CachedCurrentOAuthUser(*scope)
-}
-
-// Currently a stub.
-// See https://github.com/crhym3/go-endpoints/issues/2
-func CurrentUserWithScope(c appengine.Context, scope string) (*user.User, error) {
-	return user.CurrentOAuth(c, scope)
+	return c.CurrentOAuthUser(scope)
 }
